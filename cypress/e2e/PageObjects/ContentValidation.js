06274531630a4cbe5b05cbc851fc9bcc06498781
Vineet Singh <0717vineet@gmail.com>
2025-02-16 18:44:58 +0530

    Update ContentValidation.js

--- cypress/e2e/PageObjects/ContentValidation.js
+++ cypress/e2e/PageObjects/ContentValidation.js
@@ -10,9 +10,9 @@ class Content_Validation{
 
     checkDescription(){
         cy.get('[class="nav-link dropdown-toggle"]').click();
-        cy.get('[class="dropdown-item"]').contains('Hand Tools').click();//dropdown
-        cy.get('(//h5[@data-test="product-name"])[1]').click();
-        cy.get('[id="description"]').should("have.text"," Nunc vulputate, orci at congue faucibus, enim neque sodales nulla, nec imperdiet augue odio vel nibh. Etiam auctor, ligula quis gravida dictum, mi massa commodo ante, sollicitudin pulvinar nulla justo hendrerit lacus. Vivamus rutrum pharetra molestie. Fusce tristique odio tristique, elementum est eget, porttitor diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas at ligula sed sapien porta pretium. Aenean cursus, magna in blandit consectetur, libero orci aliquet eros, et maximus nunc est eu dolor. Aenean non pellentesque eros. In sodales orci eget orci fringilla, vitae feugiat elit porta. Etiam aliquam, mi pretium tempus mattis, mauris ipsum gravida risus, at tempor nulla ipsum molestie ligula. Ut placerat, urna sit amet tincidunt volutpat, ex orci luctus purus, nec laoreet dolor sapien vel erat.");
+        cy.get('[class="dropdown-item"]').contains('Hand Tools').click();
+        cy.get('[class="card-title"]').eq(0).click({force:true});
+        cy.get('[id="description"]').should("be.visible");
     }
 }
 export default Content_Validation;
